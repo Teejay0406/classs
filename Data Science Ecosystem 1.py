ccb45ee845e443dccb4fddedbe5fe7ac2020c1f7
Teejay0406 <157185301+Teejay0406@users.noreply.github.com>
2024-01-22 03:28:11 +0000

    Add files via upload

---
+++ Data Science Ecosystem 1.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# Exercise 1: Create a Jupyter Notebook
+# 
+# Create a new Jupyter notebook called DataScienceEcosystem.ipynb
+# 
+
+# Exercise 2: Create markdown cell with title of the notebook
+# 
+# Create a markdown cell with the title Data Science Tools and Ecosystem using H1 style heading. Take a screenshot of the markdown cell and name it as 2-title.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# Data Science Tools and Ecosystem
+
+# Exercise 3 - Create a markdown cell for an introduction
+# 
+# Write an introductory sentence about the notebook such as the follows:
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized. Take a screenshot of the markdown cell and name it as 3-intro.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Exercise 4 - Create a markdown cell to list data science languages
+# 
+# Start the cell with an overview line such as:
+# 
+# Some of the popular languages that Data Scientists use are: Then create an ordered list (i.e. numbered) listing 3 (or more) commonly used languages for data science. Take a screenshot of the markdown cell and name it as 4-dslanguages.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# Some of the popular languages that Data Scientists used are:
+# 
+# 1. Python
+# 2. R
+# 3. Julia
+# 4. C++
+# 5. Java Script
+# 
+
+# Exercise 5 - Create a markdown cell to list data science libraries
+# 
+# Add an overview line to the cell like:
+# 
+# Some of the commonly used libraries used by Data Scientists include: Below this line add an ordered list listing 3 (or more) commonly used libraries in data science. Take a screenshot of the markdown cell and name it as 5-dslibraries.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Numpy
+# 2. pandas
+# 3. matplotlib
+# 4. seaborn
+# 5. scikit-learn
+# 6. caret
+# 7. TensorFlow
+# 
+
+# Exercise 6 - Create a markdown cell with a table of Data Science tools
+# 
+# Create a single column table in this cell with the first row containing the header Data Science Tools. The subsequent three rows in the table should indicate three development environment open source tools used in data science.
+# 
+# |Data Science tools|
+# |------------------|
+# |Jupyter Notebooks|
+# |RStudio|
+# |VSCode (Visual Studio Code)|
+# 
+
+# Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+# 
+# Add a line in this cell with H3 style heading with text like:
+# 
+# Below are a few examples of evaluating arithmetic expressions in Python. Take a screenshot of the markdown cell and name it as 7-introarithmetic.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+# - 2+7
+# - 30*5
+# - 14-5
+# - 30/6
+# 
+
+# Exercise 8 - Create a code cell to multiply and add numbers
+# 
+# In this code cell evaluate the expression (3*4)+5. Insert a comment line before the expression to explain the operation e.g. This a simple arithmetic expression to mutiply then add integers. Then execute the cell to ensure the expression returns the expected output of 17. Take a screenshot of the code cell with output and name it as 8-multiplyandaddintegers.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# 
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers.
+(3*4)+5
+17
+
+
+# Exercise 9 - Create a code cell to convert minutes to hours
+# 
+# In this code cell write an expression that converts 200 minutes into hours. Insert a comment line before the expression to explain the operation e.g. This will convert 200 minutes to hours by diving by 60. Run the cell to evaluate the expression.
+# 
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+minutes = 200
+hours = minutes / 60
+
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# Exercise 10 - Insert a markdown cell to list Objectives
+# 
+# Below the introduction cell created in Exercise 3, insert a new markdown cell to list the objectives that this notebook covered (i.e. some of the key takeaways from the course). In this new cell start with an introductory line titled: Objectives: in bold font. Then using an unordered list (bullets) indicate 3 to 5 items covered in this notebook, such as List popular languages for Data Science. Take a screenshot of the markdown cell and name it as 10-objectives.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized
+# 
+# Objectives:
+# 
+# R studio
+# Jupyternotebook and Jupyterlab
+# Github
+# Libraries, APIs, Datasets and Models
+# 
+
+# Exercise 11 - Create a markdown cell to indicate the Author's name
+# 
+# In this markdown cell markdown cell include the following text Author in H2 style heading. Include your name as regular text below the word Author. Take a screenshot of the markdown cell and name it as 11-authordetails.png (Images can be saved with either the .jpg or .png extension.)
+# 
+# ## Author
+# 
+# Olatunji Olagunju
+
+# In[ ]:
+
+
+
+
